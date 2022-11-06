69d9e0f9aa77806fc96753a0079d82cf90a3def5
Asahel Enock Kigen <kirwaasahel@gmail.com>
2022-11-06 23:08:12 +0300

    [UPDATE]: Onboarding process reponsive update

--- components/pages/home/onboarding.js
+++ components/pages/home/onboarding.js
@@ -6,13 +6,13 @@ const Onboarding = () => {
             <div style={{ display:"flex", justifyContent:"center", alignItems: "center", backgroundColor: "#fff" }} className="bg-white flex justify-center items-center">
                 <div className="onboarding__container">
                     <div className="row section-header" data-aos="fade-up">
-                        <div className="">
+                        <div className="onboarding__container">
                             <h3 style={{ color: "black", fontSize: "3.75rem", lineHeight: "1", fontWeight:"bold" }}>Our onboarding process</h3>
                             <div style={{padding: "0.75rem 0"}} className="subhead__underline">
                                 <div className="subhead__underline__body"></div>
                             </div>
                             <div style={{margin: "0 8rem"}}>
-                                <div style={{margin: "2.5rem 0", textAlign: "justify"}}>
+                                <div style={{margin: "2.5rem 0",}}>
                                     <p>
                                         We have deployed a BD process that allows us to
                                         serve our clients in the best way we know how to
